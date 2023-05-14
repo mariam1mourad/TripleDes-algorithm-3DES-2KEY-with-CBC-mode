a23a0cfe5f3bba647191a5742ed840ba229e73be
Mediinnaa <121581992+Mediinnaa@users.noreply.github.com>
2023-05-14 16:09:30 +0200

    Update TripleDes.py

--- TripleDes.py
+++ TripleDes.py
@@ -14,3 +14,17 @@ while True:
 
     tdes_key = DES3.adjust_key_parity(key_hash)
     cipher = DES3.new(tdes_key, DES3.MODE_EAX, nonce=b'0')
+    
+    
+     with open(file_path, 'rb') as input_file:
+        file_bytes = input_file.read()
+
+        if operation == '1':
+            # Encrypt
+            new_file_bytes = cipher.encrypt(file_bytes)
+        else:
+            # Decrypt
+            new_file_bytes = cipher.encrypt(file_bytes)
+
+    with open(file_path, 'wb') as output_file:
+        output_file.write(new_file_bytes)
